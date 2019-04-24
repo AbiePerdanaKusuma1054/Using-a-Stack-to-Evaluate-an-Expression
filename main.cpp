696772d7bf2f22641f79616b5db8f0beec2ff9ee
Eggy Bintang Negara 1013 <48612244+eggybintangn13@users.noreply.github.com>
2019-04-24 23:39:54 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ struct node{
 	char element;
 	node *next;
 };
-
+//Deklarasi Stack
 class stack{
 	private:
 		node *head=NULL;
@@ -62,7 +62,7 @@ class stack{
 			}
 		}    
 };
-
+//Fungsi untuk mengecek nilai operator *dan/ > +dan-
 int cek(char cekoprtr){	
 	if(cekoprtr=='*'||cekoprtr=='/')
 		return 2;
@@ -71,66 +71,74 @@ int cek(char cekoprtr){
 }
 
 int main(){
-	stack S;
+	stack S; //Stack yang bernama S
 	int i=0,n=0,A,B,C;
-	char postfix[20];
-	char infix[20];
+	char postfix[20]; //Deklarasi postfix
+	char infix[20]; //Deklarasi infix
 	
 	cin>>infix;
-	while(infix[i]!='\0'){
-		if(isdigit(infix[i])){
-			postfix[n]=infix[i];
+	while(infix[i]!='\0'){ //Selama belum mencapai akhir dari infix
+		if(isdigit(infix[i])){ //Jika di infix ditemukan angka
+			postfix[n]=infix[i]; //Masukkan ke dalam postfix
 			n++;
 		}
-		else if(infix[i]=='('){
-			S.pushtostack(infix[i]);
+		else if(infix[i]=='('){ //Jika di infix ditemukan "("
+			S.pushtostack(infix[i]); //Masukkan ke dalam S
 		}
-		else if(infix[i]==')'){
-			while(!S.isEmpty() && S.top()!='('){
-				postfix[n]=S.popfromstack();
+		else if(infix[i]==')'){ //Jika di infix ditemukan ")"
+			while(!S.isEmpty() && S.top()!='('){ //Selama S tidak kosong DAN elemen pertama S bukan "("
+				postfix[n]=S.popfromstack(); //Keluarkan elemen pertama S lalu masukkan ke dalam postfix
 				n++;
 			}
-			S.popfromstack();
+			S.popfromstack(); //Keluarkan "(" dari S lalu buang/hilangkan
 		}
-		else{
-			if(S.isEmpty() || S.top()=='('){
-				S.pushtostack(infix[i]);
+		else{ //Jika di infix ditemukan Operator
+			if(S.isEmpty() || S.top()=='('){ //Jika S kosong ATAU elemen pertama S adalah "("
+				S.pushtostack(infix[i]); //Masukkan ke dalam S
 			}
 			else{
-				while(!S.isEmpty() && S.top()!='(' && cek(infix[i])<=cek(S.top())){
-					postfix[n]=S.popfromstack();
+				while(!S.isEmpty() && S.top()!='(' && cek(infix[i])<=cek(S.top())){ /*Selama S tidak kosong DAN elemen
+												pertama S bukan "(" DAN operator yang 
+												ditemukan nilainya lebih kecil dari
+												operator [elemen pertama] di S*/
+					postfix[n]=S.popfromstack(); //Keluarkan elemen pertama S lalu masukkan ke postfix
 					n++;
 				}
-				S.pushtostack(infix[i]);
+				S.pushtostack(infix[i]); //Masukkan operator yang ditemukan ke dalam S
 			}
 		}
 		i++;
 	}
-	while(!S.isEmpty()){
-		postfix[n]=S.popfromstack();
+	while(!S.isEmpty()){ //Selama S tidak kosong
+		postfix[n]=S.popfromstack(); //Keluarkan seluruh elemen di S lalu masukkan ke postfix
 		n++;
 	}
 	n=0;
-  	while(postfix[n]!='\0'){  
-    		if(isdigit(postfix[n])){
-				S.pushtostack(postfix[n]);
+  	while(postfix[n]!='\0'){  //Selama belum mencapai akhir dari postfix
+    		if(isdigit(postfix[n])){ //Jika di postfix ditemukan angka
+				S.pushtostack(postfix[n]); //Masukkan ke dalam S
 			}
-	  	else{
-		 		A=S.popfromstack()-'0';
-		 		B=S.popfromstack()-'0';
-				if(postfix[n]=='*'){
-					C=B*A;
-					S.pushtostack(C+'0');
+	  	else{ //Jika di postfix ditemukan operator
+		 		A=S.popfromstack()-'0'; /*Keluarkan elemen pertama  S lalu jadikan A [pengurangan dengan'0' digunakan 
+							untuk merubah elemen yang dikeluarkan dari S yg bertipe char agar nantinya dapat
+							dioperasikan]*/
+		 		B=S.popfromstack()-'0'; /*Keluarkan elemen pertama  S lalu jadikan B [pengurangan dengan '0' digunakan 
+							untuk merubah elemen yang dikeluarkan dari S yg bertipe char agar nantinya dapat 
+							dioperasikan]*/
+				if(postfix[n]=='*'){ //Jika operator yg ditemukan adalah *
+					C=B*A; //operasikan
+					S.pushtostack(C+'0'); /*Hasil dari operasinya dimasukkan ke S [penambahan dengan '0' digunakan
+								untuk merubah kembali menjadi bertipe char*/
 		  		}
-      				else if(postfix[n]=='/'){
+      				else if(postfix[n]=='/'){ //Jika operator yg ditemukan adalah /
 			 		C=B/A;
 		 			S.pushtostack(C+'0');
 		  		}
-				else if(postfix[n]=='+'){
+				else if(postfix[n]=='+'){ //Jika operator yg ditemukan adalah +
 			 		C=B+A;
 					S.pushtostack(C+'0');	
 		  		}
-				else if(postfix[n]=='-'){
+				else if(postfix[n]=='-'){ ////Jika operator yg ditemukan adalah -
 					C=B-A;
 					S.pushtostack(C+'0');
 		  		}
@@ -140,6 +148,7 @@ int main(){
 			}
 	  	n++;
   	}
-	cout<<S.popfromstack()-'0';
+	cout<<S.popfromstack()-'0'; /*tampilkan elemen yg dikeluarkan dari S [pengurangan dengan'0' digunakan untuk merubah elemen yang 
+					dikeluarkan dari S yg bertipe char*/
 	return 0;
 }
